85029a37d8db59bd50a0f846315d1cc91970fd60
Alexis-70 <alessio.cesario70@gmail.com>
2024-10-15 16:31:18 +0200

    Update iat.js

--- iat.js
+++ iat.js
@@ -244,14 +244,22 @@ function recordResponse(isCorrect) {
         const avgRT_Compatibile = (avgRT_Io_Vergogna + avgRT_NonIo_Ansia) / 2;
 const avgRT_Incompatibile = (avgRT_Io_Ansia + avgRT_NonIo_Vergogna) / 2;
 
-            // Crea una stringa con tutti i tempi di reazione
- const allReactionTimes = [
-    '- ' + reactionTimes['Io_Vergogna'].join(', '),
-    '- ' + reactionTimes['NonIo_Vergogna'].join(', '),
-    '- ' + reactionTimes['Io_Ansia'].join(', '),
-    '- ' + reactionTimes['NonIo_Ansia'].join(', ')
-].join(', '); // Unisci i tempi in una stringa separata da virgole
+// Effettua la sottrazione tra i tempi di reazione del blocco 3 (Io_Vergogna e NonIo_Ansia)
+// e i tempi del blocco 5 (NonIo_Vergogna e Io_Ansia)
+let differenceTimes = [];
 
+for (let i = 0; i < reactionTimes['Io_Vergogna'].length; i++) {
+    let difference = reactionTimes['Io_Vergogna'][i] - reactionTimes['NonIo_Vergogna'][i];
+    differenceTimes.push(difference);
+}
+
+for (let i = 0; i < reactionTimes['NonIo_Ansia'].length; i++) {
+    let difference = reactionTimes['NonIo_Ansia'][i] - reactionTimes['Io_Ansia'][i];
+    differenceTimes.push(difference);
+}
+
+// Crea una stringa con i risultati delle sottrazioni
+let differenceTimesString = differenceTimes.join(', ');
         
     // Calcola il numero totale di risposte e il numero di risposte errate
     const totalResponses = reactionTimes['Io_Vergogna'].length + reactionTimes['NonIo_Vergogna'].length + reactionTimes['Io_Ansia'].length + reactionTimes['NonIo_Ansia'].length;
@@ -285,7 +293,7 @@ const D = (avgRT_Incompatibile - avgRT_Compatibile) / sd;
         const formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSf-1xmaYc6C0_cWOJQ14_HyUP9JJwiak5X4XhKpPnJRKCBXOw/formResponse'; // Inserisci qui l'URL del tuo Google Form
         const data = {
             'entry.695362309': userName, // Sostituisci con l'ID del campo per il nome
-            'entry.222093517': allReactionTimes, // Sostituisci con l'ID del campo per il cognome
+            'entry.222093517': differenceTimesString, // Sostituisci con l'ID del campo per il cognome
             'entry.1683801057': D.toFixed(2) // Sostituisci con l'ID del campo per il punteggio D
         };
 
